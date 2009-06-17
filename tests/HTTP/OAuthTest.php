2d40336e5b3efcafdf605fbbd35f41354399df93
Jeff Hodsdon <jeff@digg.com>
2009-06-17 16:49:40 -0700

    Updated tests to use urlencoded/urldecode and added decode method.

--- tests/HTTP/OAuthTest.php
+++ tests/HTTP/OAuthTest.php
@@ -5,15 +5,24 @@ require_once 'HTTP/OAuth.php';
                                                                                 
 class HTTP_OAuthTest extends PHPUnit_Framework_TestCase        
 {
-    public function testEncode()
+    public function testURLEncode()
     {
         $raw = 'http://www.joestump.net/~foobar';
         $exp = 'http%3A%2F%2Fwww.joestump.net%2F~foobar';
         $bad = 'http%3A%2F%2Fwww.joestump.net%2F%7Efoobar';
-        $res = HTTP_OAuth::encode($raw);
+        $res = HTTP_OAuth::urlencode($raw);
         
         $this->assertEquals($exp, $res);
         $this->assertNotEquals($bad, $res);
+    }
+
+    public function testURLDecode()
+    {
+        $raw = 'http://www.joestump.net/~foobar';
+        $exp = 'http%3A%2F%2Fwww.joestump.net%2F~foobar';
+        $res = HTTP_OAuth::urldecode($exp);
+
+        $this->assertEquals($res, $raw);
     }
 }
 
