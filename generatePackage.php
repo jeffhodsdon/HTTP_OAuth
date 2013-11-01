a2dc3a3636cf7ca59a202cead32b29f842276c7e
Michael Gauthier <mike@silverorange.com>
2013-11-01 12:00:10 -0300

    Prepare for release of 0.3.0.

--- generatePackage.php
+++ generatePackage.php
@@ -32,15 +32,27 @@ $packagexml->setSummary('PEAR implementation of the OAuth 1.0a specification');
 $packagexml->setDescription('Allows the use of the consumer and provider angles of the OAuth 1.0a specification');
 
 $packagexml->setChannel('pear.php.net');
-$packagexml->setAPIVersion('0.2.0');
-$packagexml->setReleaseVersion('0.2.3');
+$packagexml->setAPIVersion('0.3.0');
+$packagexml->setReleaseVersion('0.3.0');
 
 $packagexml->setReleaseStability('alpha');
 
 $packagexml->setAPIStability('alpha');
 
-$packagexml->setNotes('* Fixed GH issue #10.  don\'t use reset() to get the first array value
-* Disabled E_DEPRECTED error logging when creating packages
+$packagexml->setNotes('API changes:
+ * added $body parameter to HTTP_OAuth_Provider::__construct()
+ * added HTTP_OAuth_Provider::setBody()
+ * renamed HTTP_OAuth_Provider::getPostData() to getBody()
+ * made HTTP_OAuth_Provider::getBody() public
+
+New features and bugs fixed:
+ * Fixed PEAR #17806. DELETE method is not supported.
+ * Fixed PEAR #18574. Avoid try-catch-rethrow.
+ * Fixed PEAR #18701. Only variables should be passed by reference.
+ * Fixed PEAR #18425. Array keys not decoded in HTTP_OAuth_Provider.
+ * Fixed PEAR #18431. Handle PUT requests better in HTTP_OAuth_Provider.
+ * Fixed PEAR #20106. rawBodyData always included in provider request.
+ * Fixed PEAR #20107. Handle multiple query params with same name as array.
 ');
 $packagexml->setPackageType('php');
 $packagexml->addRelease();
@@ -55,6 +67,10 @@ $packagexml->addMaintainer('lead',
                            'shupp',
                            'Bill Shupp',
                            'shupp@php.net');
+$packagexml->addMaintainer('developer',
+                           'gauthierm',
+                           'Michael Gauthier',
+                           'mike@silverorange.com');
 
 $packagexml->setLicense('New BSD License',
                         'http://www.opensource.org/licenses/bsd-license.php');
